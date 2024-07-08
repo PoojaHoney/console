0964a1845f2ecf2b4dd5f1fd17e53780c8e42e6f
PoojaHoney <poojahoney/tumu@gmail.com>
2024-07-08 20:36:46 +0530

    updated code

--- backend/instances/cloudProviders/gcp/credentials.py
+++ backend/instances/cloudProviders/gcp/credentials.py
@@ -3,8 +3,9 @@ from google.oauth2 import service_account as GCPSrvAcc
 def get_gcp_crds(crds: dict):
     print("crdsssss: ", crds)
     result =  GCPSrvAcc.Credentials.from_service_account_info(
-    filename="svc-acc-gcp-cloud-automation-cred.json", 
-    # crds,
-    scopes=["https://www.googleapis.com/auth/cloud-platform"])
+    # filename="svc-acc-gcp-cloud-automation-cred.json", 
+    crds,
+    scopes=["https://www.googleapis.com/auth/cloud-platform"]
+    )
     print(result)
     return result
