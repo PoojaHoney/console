7341fb2f4a41f7310c928b836484802600daa4c4
PoojaHoney <poojahoney/tumu@gmail.com>
2024-07-08 10:50:16 +0530

    updated the cors for instances service

--- backend/instances/main.py
+++ backend/instances/main.py
@@ -2,12 +2,23 @@ import uvicorn
 from fastapi import FastAPI
 from api_v1.api import api_router
 from config import settings
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(
     title=settings.SERVICE_NAME, openapi_url=f"{settings.SERVICE_BASEPATH}/{settings.SERVICE_VERSION}/openapi.json", docs_url="/docs"
 )
 
-app.include_router(api_router, prefix=f"{settings.SERVICE_BASEPATH}/{settings.SERVICE_VERSION}")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust this to your needs
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(
+    api_router, prefix=f"{settings.SERVICE_BASEPATH}/{settings.SERVICE_VERSION}")
 
 if __name__ == "__main__":
     uvicorn.run(app, host=settings.SERVICE_DOMAIN, port=settings.SERVICE_PORT)
